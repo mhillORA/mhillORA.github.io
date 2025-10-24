641bdc40782daba17a29402c56a15f0fddad813d
mhillORA <mhill@oraclinical.com>
2025-10-24 13:26:27 -0400

    Update index.js

--- api/api-function/index.js
+++ api/api-function/index.js
@@ -9,8 +9,7 @@ const client = new CosmosClient({ endpoint: COSMOS_ENDPOINT, key: COSMOS_KEY });
 const database = client.database(DATABASE_ID);
 
 module.exports = async function (context, req) {
-    // ... rest of your API code
-};
+    context.log('Processing request:', req.method, req.url);
 
     try {
         // Handle CORS
@@ -41,10 +40,10 @@ module.exports = async function (context, req) {
                     context.res.status = 201;
                     context.res.body = resource;
                 } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('studies').item(id, id).replace(req.body);
+                    const { resource } = await database.container('studies').item(id, '/id').replace(req.body);
                     context.res.body = resource;
                 } else if (req.method === 'DELETE' && id) {
-                    await database.container('studies').item(id, id).delete();
+                    await database.container('studies').item(id, '/id').delete();
                     context.res.status = 204;
                 }
                 break;
@@ -58,125 +57,15 @@ module.exports = async function (context, req) {
                     context.res.status = 201;
                     context.res.body = resource;
                 } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('sites').item(id, id).replace(req.body);
+                    const { resource } = await database.container('sites').item(id, '/id').replace(req.body);
                     context.res.body = resource;
                 } else if (req.method === 'DELETE' && id) {
-                    await database.container('sites').item(id, id).delete();
+                    await database.container('sites').item(id, '/id').delete();
                     context.res.status = 204;
                 }
                 break;
 
-            case 'patients':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('patients').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('patients').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('patients').item(id, id).replace(req.body);
-                    context.res.body = resource;
-                }
-                break;
-
-            case 'crcs':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('crcs').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('crcs').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('crcs').item(id, id).replace(req.body);
-                    context.res.body = resource;
-                } else if (req.method === 'DELETE' && id) {
-                    await database.container('crcs').item(id, id).delete();
-                    context.res.status = 204;
-                }
-                break;
-
-            case 'events':
-            case 'crc_events':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('events').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('events').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('events').item(id, id).replace(req.body);
-                    context.res.body = resource;
-                } else if (req.method === 'DELETE' && id) {
-                    await database.container('events').item(id, id).delete();
-                    context.res.status = 204;
-                }
-                break;
-
-            case 'schedules':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('schedules').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('schedules').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('schedules').item(id, id).replace(req.body);
-                    context.res.body = resource;
-                } else if (req.method === 'DELETE' && id) {
-                    await database.container('schedules').item(id, id).delete();
-                    context.res.status = 204;
-                }
-                break;
-
-            case 'surveys':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('surveys').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('surveys').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('surveys').item(id, id).replace(req.body);
-                    context.res.body = resource;
-                } else if (req.method === 'DELETE' && id) {
-                    await database.container('surveys').item(id, id).delete();
-                    context.res.status = 204;
-                }
-                break;
-
-            case 'roles':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('roles').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('roles').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                } else if (req.method === 'PUT' && id) {
-                    const { resource } = await database.container('roles').item(id, id).replace(req.body);
-                    context.res.body = resource;
-                } else if (req.method === 'DELETE' && id) {
-                    await database.container('roles').item(id, id).delete();
-                    context.res.status = 204;
-                }
-                break;
-
-            case 'training-types':
-                if (req.method === 'GET') {
-                    const { resources } = await database.container('training_types').items.readAll().fetchAll();
-                    context.res.body = resources;
-                } else if (req.method === 'POST') {
-                    const { resource } = await database.container('training_types').items.create(req.body);
-                    context.res.status = 201;
-                    context.res.body = resource;
-                }
-                break;
-
+            // Add other containers as needed...
             default:
                 context.res.status = 404;
                 context.res.body = { error: 'Resource not found' };
